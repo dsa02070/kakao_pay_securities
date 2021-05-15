b3bf730dd3aff8c3fe92334ea85feb742e094ac0
dsa02070 <82920762+dsa02070@users.noreply.github.com>
2021-05-16 00:32:09 +0900

    sub html update
    sub html update

---
+++ js/introduce.js
@@ -0,0 +1,26 @@
+$(function () {
+    $(window).scroll(function () {
+        let scTop = $(this).scrollTop();
+        let firstOffset = $('.first').offset().top;
+        let secondOffset = $('.second').offset().top;
+        let thirdOffset = $('.third').offset().top;
+        let fourthOffset = $('.fourth').offset().top;
+        let infoOffset = $('.info').offset().top;
+
+        if (firstOffset - 374 <= scTop) {
+            $('.first span, .first h3, .first img').addClass('upMove');
+        }
+        if (secondOffset - 600 <= scTop) {
+            $('.second h3, .second p').addClass('upMove')
+        }
+        if (thirdOffset - 525 <= scTop) {
+            $('.third h3, .box').addClass('upMove');
+        }
+        if (fourthOffset - 500 <= scTop) {
+            $('.fourth h3, .map_api').addClass('upMove');
+        }
+        if (infoOffset - 600 <= scTop) {
+            $('.info h4, .info p').addClass('upMove');
+        }
+    })
+})
