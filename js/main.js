3d69bc548212665d0f57747efb59aee24678c2dd
dsa02070 <82920762+dsa02070@users.noreply.github.com>
2021-05-20 00:12:45 +0900

    small update
    small update

--- js/main.js
+++ js/main.js
@@ -1,15 +1,16 @@
 $(function () {
     // section scroll event
     $(window).scroll(function () {
+        const addTop = 500;
         let scTop = $(this).scrollTop();
         let firstOffset = $('#first').offset();
         let secondOffset = $('#second').offset();
 
-        if (firstOffset.top - 500 <= scTop) {
+        if (firstOffset.top <= scTop + addTop) {
             $('#first h3, #first p, #first a').addClass('first_on');
             $('#first').addClass('firstAni_on')
         }
-        if (secondOffset.top - 500 <= scTop) {
+        if (secondOffset.top <= scTop + addTop) {
             $('#second').addClass('second_bg_on');
             $('#second h3, #second a').addClass('second_on');
         }
