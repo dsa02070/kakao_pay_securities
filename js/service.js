89075fe21970559a0a6e4fdb09e5c05641c6eb57
dsa02070 <82920762+dsa02070@users.noreply.github.com>
2021-05-27 22:17:39 +0900

    service.js 코드 줄이기
    each, eq()로 코드줄이기

--- js/service.js
+++ js/service.js
@@ -3,31 +3,20 @@ $(function () {
     $('.tab_btn ul').on('click', 'li', function () {
         $('.tab_btn ul li').removeClass('tab_on');
         $(this).addClass('tab_on');
-    })
+    });
 
-    // 버튼 클릭 시 컨텐츠 변화
-    $('.list_wrap > div').css('display', 'none'); // 초기 상태
-    $('.first_list').css('display', 'block');
-    $('.r_list').click(function () {
-        $('.list_wrap > div').css('display', 'none');
-        $('.first_list').css('display', 'block');
-    })
-    $('.s_list').click(function () {
-        $('.list_wrap > div').css('display', 'none');
-        $('.second_list').css('display', 'block');
-    })
-    $('.t_list').click(function () {
-        $('.list_wrap > div').css('display', 'none');
-        $('.third_list').css('display', 'block');
-    })
-    $('.f_list').click(function () {
-        $('.list_wrap > div').css('display', 'none');
-        $('.fourth_list').css('display', 'block');
-    })
-    $('.i_list').click(function () {
-        $('.list_wrap > div').css('display', 'none');
-        $('.fifth_list').css('display', 'block');
-    })
+    // tab_btn, list_wrap event
+    // 초기 : 첫번 째를 제외하고 제거.
+    $('.list_wrap > div:gt(0)').css('display', 'none')
+    // 버튼 클릭 시 해당 인덱스 번호에 맞는 컨텐츠 보여주기
+    $('.tab_btn ul li').each(function () {
+        let indexNum = $(this).index()
+        console.log(indexNum)
+        $('.tab_btn ul li:eq("' + indexNum + '")').click(function () {
+            $('.list_wrap > div').css('display', 'none');
+            $('.list_wrap > div:eq("' + indexNum + '")').css('display', 'block');
+        });
+    });
 
     // 소비자보호프로세스 조직도 이미지 속성 변경
     $(window).resize(function () {
@@ -36,9 +25,8 @@ $(function () {
             $('.customer img').attr('src', '../img/service/customer_mobile.jpg')
         } else {
             $('.customer img').attr('src', '../img/service/customer.jpg')
-        }
-    })
-
+        };
+    }).resize()
 
     // input placeholder 대체 코드
     $('.input_search').val('검색어를 입력하세요').css('color', '#ccc').one('focus', function () {
@@ -47,7 +35,7 @@ $(function () {
         if ($(this).val() == '') {
             $(this).val('검색어를 입력해주세요').css('color', '#ccc').one('focus', function () {
                 $(this).val('').css('color', '#222')
-            })
-        }
-    })
-})
+            });
+        };
+    });
+});
